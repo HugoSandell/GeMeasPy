HugoSandell/GeMeasPy

commit 22c9d911ea28f9215596d5cfa81ebcd84ffe18b8

ssh log: write time stamp in correct format

src/acquisition/connections.py
@@ -4,6 +4,8 @@ from typing import Any
 
 import paramiko
 
+import utilities
+
 from settings.config import LOG_FOLDER
 
 class SSHConnection():
@@ -24,7 +26,7 @@ class SSHConnection():
             raise Exception("No Active Connection")
         stdin, stdout, stderr = self.ssh.exec_command(command)
         exit_status = stdout.channel.recv_exit_status()  # wait for exit status
-        self.log.write(f"SH:{time.time().hex()}>{command.encode().hex()}<{exit_status}\n")
+        self.log.write(f"SH:{utilities.timestamp_hex()}>{command.encode().hex()}<{exit_status}\n")
         time.sleep(time_to_sleep)
         return stdin, stdout, stderr
 
@@ -32,7 +34,7 @@ class SSHConnection():
         if self.channel is None:
             raise Exception("No Active Connection")
         encoded_command = command.encode(encoding="UTF-8")
-        self.log.write(f"TM:{time.time().hex()}>{encoded_command.hex()}\n")
+        self.log.write(f"TM:{utilities.timestamp_hex()}>{encoded_command.hex()}\n")
         self.channel.send(encoded_command)
         time.sleep(time_to_sleep)
             
@@ -42,7 +44,7 @@ class SSHConnection():
         # Receive, decode, log, and return data
         rx_raw = self.channel.recv(chars)
         rx_decoded = rx_raw.decode(encoding="UTF-8", errors='replace')
-        self.log.write(f"TM:{time.time().hex()}<{rx_raw.hex()}\n")
+        self.log.write(f"TM:{utilities.timestamp_hex()}<{rx_raw.hex()}\n")
         return rx_decoded
 
     def _setup(self) -> bool:

commit b6a4c22ebd8b542dbb9dba0d859c91d084de90a4

fix hex timestamp formatting and import error caused in commit 22c9d911ea28f9215596d5cfa81ebcd84ffe18b8

src/acquisition/connections.py
@@ -4,7 +4,7 @@ from typing import Any
 
 import paramiko
 
-import utilities
+from acquisition import utilities
 
 from settings.config import LOG_FOLDER
 

src/acquisition/utilities.py
@@ -172,4 +172,4 @@ def wait(start_time: str) -> None:
 def timestamp_hex() -> str:
     nano_to_milli = 1.0 / 1_000_000
     timestamp = int(time.time_ns() * nano_to_milli)
-    return f'{timestamp:0+x}'
+    return f'{timestamp:016x}'