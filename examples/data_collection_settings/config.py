6f32028bb90fd89f8d094636a9f60d4fc662f4f2
Hugo Sandell <hugsan@kth.se>
2025-09-23 00:26:38 +0200

    update log folder in example config

--- examples/data_collection_settings/config.py
+++ examples/data_collection_settings/config.py
@@ -1,8 +1,7 @@
 # Program Configuration
-# Relative paths are resolved relative to the src directory.
 
 # Folder to contain log files
-LOG_FOLDER = r"../logs"
+LOG_FOLDER = r"./logs"
 
 # Path to projects folder on Terrameter
 TERRAMETER_PROJECTS_FOLDER = r"/media/mmcblk0p1/projects"
