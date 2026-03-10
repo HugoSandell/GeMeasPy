c397d317a6ade708854519cde69c456eb4dede65
August Wikerfors <git@augustwikerfors.se>
2026-03-10 19:35:29 +0100

    refactor: use fixtures instead of calling setup in test

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,7 +1,19 @@
+import pytest
+
 from acquisition import main
 from tests import setup_config, setup_task_files
 
 
-def test_main(test_case):
+@pytest.fixture
+def config(test_case):
     setup_config.setup(test_case)
-    main.main([main.__file__] + setup_task_files.resolve_task_files(test_case))
+    yield
+
+
+@pytest.fixture
+def task_files(test_case):
+    yield setup_task_files.resolve_task_files(test_case)
+
+
+def test_main(test_case, config, task_files):
+    main.main([main.__file__] + task_files)
