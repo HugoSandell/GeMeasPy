f4451f653bde7d1848e02f376f6e8e62224a4d5d
Hugo Sandell <hugossandell@gmail.com>
2026-03-15 19:11:38 +0100

    refactor: rename log file to be more universally applicable

--- gemeaspy/tests/logging.py
+++ gemeaspy/tests/logging.py
@@ -4,7 +4,7 @@ from logging import debug, critical, info, error, warning
 def configure_logs():
     from os.path import dirname, join, abspath
     import gemeaspy
-    log_file_path = dirname(gemeaspy.__file__) + "/../log/test_generation.log" 
+    log_file_path = dirname(gemeaspy.__file__) + "/../log/gemeaspy_tests.log" 
     log_format = "%(asctime)s %(levelname)-6s [%(module)-12s:%(lineno)s] %(message)s"
     log_date_format = "%Y-%m-%d %H:%M:%S"
     _logging.basicConfig(
