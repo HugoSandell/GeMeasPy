27995b303bbf0956778f42690c1299b6a34a0666
Hugo Sandell <hugossandell@gmail.com>
2026-03-15 19:19:50 +0100

    use new logging object instead of configuring root logger

--- gemeaspy/tests/logging.py
+++ gemeaspy/tests/logging.py
@@ -1,18 +1,24 @@
 import logging as _logging
-from logging import debug, critical, info, error, warning 
+
+logger = _logging.Logger("gemeaspy_tests", _logging.DEBUG)
+
+debug = logger.debug
+critical = logger.critical
+info = logger.info
+error = logger.error
+warning = logger.warning
 
 def configure_logs():
-    from os.path import dirname, join, abspath
+    from os.path import dirname
     import gemeaspy
+    
     log_file_path = dirname(gemeaspy.__file__) + "/../log/gemeaspy_tests.log" 
-    log_format = "%(asctime)s %(levelname)-6s [%(module)-12s:%(lineno)s] %(message)s"
+    log_format = "%(asctime)s %(levelname)-6s [%(module)s:%(lineno)s] %(message)s"
     log_date_format = "%Y-%m-%d %H:%M:%S"
-    _logging.basicConfig(
-        level=_logging.DEBUG,
-        filemode="a",
-        filename=log_file_path, 
-        format=log_format, 
-        datefmt=log_date_format,
-    )
+    
+    general_formatter = _logging.Formatter(log_format, log_date_format)
+    tests_handler = _logging.FileHandler(log_file_path)
+    tests_handler.setFormatter(general_formatter)
+    logger.addHandler(tests_handler)
 
 configure_logs()
