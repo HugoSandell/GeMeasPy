7152a7984586d1dcdbb0e5eb2e7d955e8855baf1
Hugo Sandell <hugossandell@gmail.com>
2026-03-23 16:54:04 +0100

    (acquisition) Handle fatal ChannelExceptions gracefully

--- gemeaspy/acquisition/main.py
+++ gemeaspy/acquisition/main.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from paramiko import ChannelException
+
 from gemeaspy.acquisition.instruments import Terrameter
 from gemeaspy.acquisition.utilities import read_monitoring_tasks
 
@@ -19,14 +21,19 @@ def run_acquisition(argv: list[str]):
     nargs = len(argv)
     if nargs == 1:
         task_file = None
-        raise Exception("No task file provided")
-    elif nargs == 2:
-        task_file = argv[1]
-        run_task_file(task_file)
-    else:
-        task_files = argv[1:]
-        for task_file in task_files:
+        raise SystemExit("No task file provided")
+    try: 
+        if nargs == 2:
+            task_file = argv[1]
             run_task_file(task_file)
+        else:
+            task_files = argv[1:]
+            for task_file in task_files:
+                run_task_file(task_file)
+    except ChannelException as e:
+        print("Failed to create SSH shell channel to Terrameter!")
+        logging.info(f"ChannelException - [{e.code}] {e.text}")
+        return False
 
 if __name__ == "__main__":
     run_acquisition(sys.argv)
