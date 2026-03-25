3a2fbee5ac3195f754b121b3a0451a04d6e4048e
Hugo Sandell <hugossandell@gmail.com>
2026-03-25 16:49:18 +0100

    (oracle) add stubs for connection_port and task properties

--- gemeaspy/tests/oracle.py
+++ gemeaspy/tests/oracle.py
@@ -1,38 +1,75 @@
 """This module is responsible for reviewing test execution data and determining whether or not a failure has occured"""
 
 from dataclasses import dataclass
+import re
 
 from gemeaspy.tests import _logging
 from gemeaspy.tests.parameter_spec import AcquisitionParameterSpec
 from gemeaspy.tests.setup_config import ConfigState
-from gemeaspy.tests.test_case import AcquisitionTestCase
+from gemeaspy.tests.test_case import AcquisitionTestCase, TestCase
 
+# Regular expressions for matching a "class" of paramters
+RE_TASK = re.compile(r"taskfile(?P<file>\d+)_task(?P<task>\d+)_(?P<property>spread|protocol|name|settings|spacing)")
 
 @dataclass
 class OracleResult:
     ok: bool
     msg: str = ""
+    def __bool__(self) -> bool:
+        return self.ok
 
+# Common checks
+def _find_stdout_error_message(stdout: str) -> str | None:
+    """Checks returns first line with reported error, or None if one wasn't found"""
+    lines = stdout.splitlines()
+    error_lines = list(filter(lambda s: "Error: " in s, lines))
+    if len(error_lines) <= 0:
+        return None
+    return error_lines[0]
 
+def _was_project_transferred() -> bool:
+    """Checks whether the project has been transferred correctly"""
+    raise NotImplementedError()
+    
+# Evaluators
 def _evaluate_any(config_state: ConfigState) -> OracleResult:
     """Checks properties that should hold for all cases"""
     if config_state.data_invalid_is_modified():
         return OracleResult(False, "SUT modified file configured as LOCAL_PATH_TO_DATA")
-
     return OracleResult(True)
 
-
-def _evaluate_valid(test_data, stdout: str, stderr:str) -> OracleResult:
-    # Find any and all faulty states
-    
+def _evaluate_valid(test_data: TestCase, stdout: str, stderr:str) -> OracleResult:
+    """Checks that results are consistent with valid inputs"""
+    # Find any faulty states
     err_pos = stdout.find("Error: ")
     if err_pos >= 0:
         _logging.info(f"SUT failed with stdout:\n{stdout}\nstderr:\n{stderr}")
         return OracleResult(False, stdout[err_pos:].splitlines()[0])
-    
     # Looks clean
     return OracleResult(True)
+    
+def _evaluate_port(test_data: AcquisitionTestCase, stdout: str, stderr:str) -> OracleResult:
+    """Bad port number"""
+    if _find_stdout_error_message(stdout):
+        return OracleResult(True)
+    value = test_data.parameters.connection_port
+    return OracleResult(False, f"No error message found for invalid connection port value {repr(value)}")
 
+def _evaluate_task_property(test_case: AcquisitionTestCase, file: int, task: int, property: str) -> OracleResult:
+    """Any invalid property of a task"""
+    parameter_name = f"taskfile{file}_task{task}_{property}"
+    match property:
+        case "name":
+            raise NotImplementedError(f"Invalid task property {property}={repr(test_case.parameters[parameter_name])} not implemented in Oracle.")
+        case "settings":
+            raise NotImplementedError(f"Invalid task property {property}={repr(test_case.parameters[parameter_name])} not implemented in Oracle.")
+        case "spread":
+            raise NotImplementedError(f"Invalid task property {property}={repr(test_case.parameters[parameter_name])} not implemented in Oracle.")
+        case "protocol":
+            raise NotImplementedError(f"Invalid task property {property}={repr(test_case.parameters[parameter_name])} not implemented in Oracle.")
+        case "spacing":
+            raise NotImplementedError(f"Invalid task property {property}={repr(test_case.parameters[parameter_name])} not implemented in Oracle.")
+    return OracleResult(True)
 
 def evaluate_test(
     test_data: AcquisitionTestCase,
@@ -42,7 +79,7 @@ def evaluate_test(
     stderr: str,
 ) -> OracleResult:
     result = _evaluate_any(config_state)
-    if not result.ok:
+    if not result:
         return result
 
     param_spec = AcquisitionParameterSpec()
@@ -56,6 +93,13 @@ def evaluate_test(
     match invalid_parameter:
         case None:
             return _evaluate_valid(test_data, stdout, stderr)
+        case "connection_port":
+            return _evaluate_port(test_data, stdout, stderr)
+        case p if (match := re.match(RE_TASK, p)) != None:
+            file = int(match.group("file"))
+            task = int(match.group("task"))
+            property = str(match.group("property"))
+            return _evaluate_task_property(test_data, file, task, property)
         case X:
             raise NotImplementedError(f"Invalid value {invalid_parameter}={repr(test_data.parameters[invalid_parameter])} not implemented in Oracle.")
     
