c385636dd2231bb0cea82443e42f725d375ce537
Hugo Sandell <hugossandell@gmail.com>
2026-04-07 16:02:02 +0200

    Read configuration from environment variables with higher priority than config.py

--- gemeaspy/acquisition/__main__.py
+++ gemeaspy/acquisition/__main__.py
@@ -3,9 +3,18 @@ import sys
 import traceback
 
 from gemeaspy.acquisition.main import logger
+from gemeaspy.settings import config as _config
 from . import run_acquisition
 
 if __name__ == "__main__":
+    # Read environment variables
+    for _var in _config.__dict__:
+        if not hasattr(_config, _var) or _var not in os.environ:
+            continue
+        try:
+            setattr(_config, _var, type(_var)(os.environ[_var]))
+        except Exception:
+            print(f"Ignoring invalid environment variable {_var}='{os.environ[_var]}'")
     try:
         exit_code = run_acquisition(sys.argv)
         sys.exit(exit_code)
