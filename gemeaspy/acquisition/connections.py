4e88bf1a0fcadbefd214b648a5d372f8374d060b
August Wikerfors <git@augustwikerfors.se>
2026-04-13 17:53:41 +0200

    Add debug logging of SSH data
    Based on `test_develoment` branch.
    
    Co-authored-by: Hugo Sandell <hugossandell@gmail.com>

--- gemeaspy/acquisition/connections.py
+++ gemeaspy/acquisition/connections.py
@@ -1,7 +1,9 @@
+import os
 from typing import Any
 
 import paramiko
 
+import gemeaspy
 from gemeaspy.acquisition import utilities
 from gemeaspy.acquisition.error import ConfigFileError, SSHConnectionError
 from gemeaspy.settings import config
@@ -16,6 +18,7 @@ class SSHConnection():
         self.ssh = None
         self.channel = None
         self.connected = self._setup()
+        self.debug_log = _DebugLogger()
 
     def is_ready(self) -> bool:
         """Returns true iff this connection is ready to be used"""
@@ -44,20 +47,22 @@ class SSHConnection():
         if not self.is_ready() or self.ssh == None:
             raise SSHConnectionError("Tried to send shell command with no active connection.", self.params)
         stdin, stdout, stderr = self.ssh.exec_command(command)
-        _ = stdout.channel.recv_exit_status()  # wait for exit status
+        exit_status = stdout.channel.recv_exit_status()  # wait for exit status
+        self.debug_log.shell(command, exit_status)
         utilities.sleep_unless_testing(time_to_sleep)
         return stdin, stdout, stderr
 
     def send_command_terrameter_software(self, command: str, time_to_sleep: int = 5) -> None:
         if not self.is_ready() or self.channel == None:
             raise SSHConnectionError("Tried to send Terrameter software command with no active connection.", self.params)
-        self.channel.send(command.encode(encoding="UTF-8"))
+        self.channel.send(self.debug_log.send(command.encode(encoding="UTF-8")))
         utilities.sleep_unless_testing(time_to_sleep)
-            
 
     def read_channel_buffer(self, chars) -> str:
         if self.is_ready() and self.channel != None:
-            return self.channel.recv(chars).decode(encoding="UTF-8")
+            return self.debug_log.recv(self.channel.recv(chars)).decode(
+                encoding="UTF-8"
+            )
         raise SSHConnectionError("Tried to read channel buffer with no active connection.", self.params)
 
     def _setup(self) -> bool:
@@ -90,3 +95,28 @@ class SSHConnection():
                 return transport.getpeername()[0]
         raise SSHConnectionError("Tried to get ip, but there is no active connection.", self.params)
 
+
+class _DebugLogger:
+    def __init__(self):
+        if "DEBUG" in os.environ:
+            log_dir = os.path.normpath(f"{os.path.dirname(gemeaspy.__file__)}/../log")
+            os.makedirs(log_dir, exist_ok=True)
+            self.logfile = open(os.path.join(log_dir, "ssh.log"), "a")
+        else:
+            self.logfile = None
+
+    def shell(self, command: str, exit_status: int):
+        if self.logfile:
+            self.logfile.write(
+                f"SH:{utilities.timestamp_hex()}>{command.encode().hex()}<{exit_status}\n"
+            )
+
+    def send(self, data: bytes) -> bytes:
+        if self.logfile:
+            self.logfile.write(f"TM:{utilities.timestamp_hex()}>{data.hex()}\n")
+        return data
+
+    def recv(self, data: bytes) -> bytes:
+        if self.logfile:
+            self.logfile.write(f"TM:{utilities.timestamp_hex()}<{data.hex()}\n")
+        return data
