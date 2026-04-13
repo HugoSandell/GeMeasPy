60dee8f51ae11daf60c248086c4878d1290253ff
Hugo Sandell <hugossandell@gmail.com>
2026-04-13 15:37:53 +0200

    add more useful imports to generator __init__.py

--- gemeaspy/tests/generator/__init__.py
+++ gemeaspy/tests/generator/__init__.py
@@ -1,1 +1,4 @@
-from test_generation import generate_covering_array, generate_random_data
+from .parameter_spec import ParameterSpec, AcquisitionParameterSpec, ACQUISITION_CONSTRAINTS, ACQUISITION_PARAM_SPEC
+from .constraint import Constraint
+from .test_case import TestCase, AcquisitionTestCase
+from .test_generation import generate_covering_array, generate_random_data
