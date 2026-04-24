5dce578942f4b167ddb7a75754abf12ae2906cbe
August Wikerfors <git@augustwikerfors.se>
2026-04-24 19:56:59 +0200

    fix taskfile constraints: use valid value as argument base case and constrain all parameters if unused

--- gemeaspy/tests/generator/parameter_spec.py
+++ gemeaspy/tests/generator/parameter_spec.py
@@ -239,6 +239,14 @@ def _base_case_constraints(
         yield Constraint(antecedent + consequent)
 
 
+def _constraints_for_unused_taskfile(taskfile_no: int) -> Generator[Constraint]:
+    return _base_case_constraints(
+        f'num_args < {taskfile_no} || arg_taskfile{taskfile_no} = "{obj2acts(INVALID_FILE)}"',
+        f"taskfile{taskfile_no}",
+        ("number_of_tasks", "number_of_tasks_error", "relay_type"),
+    )
+
+
 def _constraints_for_unused_task(
     n_param: str, task_param_prefix: str, task_no: int
 ) -> Generator[Constraint]:
@@ -252,16 +260,14 @@ def _constraints_for_unused_task(
 
 # TODO: Roll constraints into parameter spec?
 ACQUISITION_CONSTRAINTS: list[Constraint] = [
+    Constraint(f'num_args < 1 => arg_taskfile1 = "{obj2acts(VALID_TASKFILE1)}"'),
+    Constraint(f'num_args < 2 => arg_taskfile2 = "{obj2acts(VALID_TASKFILE2)}"'),
+    *_constraints_for_unused_taskfile(1),
+    *_constraints_for_unused_taskfile(2),
     *_constraints_for_unused_task("taskfile1_number_of_tasks", "taskfile1_task1", 1),
     *_constraints_for_unused_task("taskfile1_number_of_tasks", "taskfile1_task2", 2),
     *_constraints_for_unused_task("taskfile2_number_of_tasks", "taskfile2_task1", 1),
     *_constraints_for_unused_task("taskfile2_number_of_tasks", "taskfile2_task2", 2),
-    Constraint(f'num_args < 1 => arg_taskfile1 = "{obj2acts(INVALID_FILE)}"'), # Since it will not be included anyway
-    Constraint(f'num_args < 2 => arg_taskfile2 = "{obj2acts(INVALID_FILE)}"'), # Since it will not be included anyway
-    Constraint(f'arg_taskfile1 = "{obj2acts(INVALID_FILE)}" => taskfile1_number_of_tasks = 0'),
-    Constraint(f'arg_taskfile2 = "{obj2acts(INVALID_FILE)}" => taskfile2_number_of_tasks = 0'),
-    Constraint(f'num_args < 1 || arg_taskfile1 = "{obj2acts(INVALID_FILE)}" => taskfile1_number_of_tasks_error = "{obj2acts("CORRECT")}"'),
-    Constraint(f'num_args < 2 || arg_taskfile2 = "{obj2acts(INVALID_FILE)}"  => taskfile2_number_of_tasks_error = "{obj2acts("CORRECT")}"'),
 ]
 
 if __name__=="__main__":
