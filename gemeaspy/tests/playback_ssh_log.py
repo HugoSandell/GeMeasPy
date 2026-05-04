b1ea6756e9aefa47ca7d42da3277290acf395d87
August Wikerfors <git@augustwikerfors.se>
2026-05-04 04:48:19 +0200

    refactor: use `_sgr` module in `playback_ssh_log`

--- gemeaspy/tests/playback_ssh_log.py
+++ gemeaspy/tests/playback_ssh_log.py
@@ -2,11 +2,7 @@ import argparse
 import time
 
 from gemeaspy.tests import ssh_log_format
-
-FG_BLUE = "\x1b[34m"
-FG_CYAN = "\x1b[36m"
-FG_GRAY = "\x1b[90m"
-FG_DEFAULT = "\x1b[39m"
+from gemeaspy.tests._sgr import CLR_BLUE_FG, CLR_CYAN_FG, CLR_GRAY_FG, with_sgr
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -37,18 +33,19 @@ if __name__ == "__main__":
             time.sleep(sleep_duration)
 
         decoded_data = entry["data"].decode(errors="replace")
-        prefix = ""
-        suffix = FG_DEFAULT
+        style = None
+        suffix = ""
 
         match entry["type"], entry["direction"]:
             case "SH", ">":
-                prefix = FG_CYAN
+                style = CLR_CYAN_FG
                 suffix = (
-                    f"{FG_GRAY} exited with status {entry['exit_status']}{suffix}\n"
+                    with_sgr(f" exited with status {entry['exit_status']}", CLR_GRAY_FG)
+                    + "\n"
                 )
             case "TM", ">":
-                prefix = FG_BLUE
+                style = CLR_BLUE_FG
             case "TM", "<":
-                suffix = ""
+                pass
 
-        print(prefix + decoded_data + suffix, end="")
+        print(with_sgr(decoded_data, style) + suffix, end="")
