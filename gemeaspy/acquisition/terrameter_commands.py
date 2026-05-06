c8d30c8b66c07a2e46b765cde5ce130fb26d612d
Hugo Sandell <hugossandell@gmail.com>
2026-05-06 19:31:55 +0200

    (acquisition) remove unneeded print statement

--- gemeaspy/acquisition/terrameter_commands.py
+++ gemeaspy/acquisition/terrameter_commands.py
@@ -218,7 +218,6 @@ def transfer_recursive(sftp: SFTPClient, remotepath: str | PurePosixPath, localp
         path_full_remote = remotepath.joinpath(path)
         path_full_local = localpath.joinpath(path)
         try:
-            print(f"{path_full_remote!r} -> {path_full_local!r}", file=sys.stderr)
             if not (os.path.exists(path_full_local.parent)):
                 print(f"{path_full_local.parent!r} doesn't exist locally")
             try: 
